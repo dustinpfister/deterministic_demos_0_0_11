7fd6807369d1503b1bc95710a22e3b1c5d67a4ab
dustinpfister <dustin.pfister@gmail.com>
2017-08-07 09:13:43 -0400

    radiusAjust

--- demos/circles/system.js
+++ demos/circles/system.js
@@ -4,16 +4,26 @@ ds.setup({
 
     maxFrame : 75,
 
-    radius : 100,
+    radius : 20,
     radian : 0,
     cx : 160,
     cy : 120,
+
+    // circles
+    size : 5,
+    radiusAjust : 0,
+    maxSize : 20,
+
     count : 1,
 
     // what to find an a for frame basis
     forFrame : function (state) {
 
         this.radian = Math.PI * 2 * state.per;
+        this.radius = 40 + 80 * state.bias;
+
+        this.radiusAjust = -this.radius * state.bias;
+        this.size = 5 + (this.maxSize - 5) * state.bias;
 
     },
 
@@ -31,15 +41,22 @@ ds.setup({
         ctx.stroke();
 
         //circles
+        ctx.fillStyle = '#ff0000';
         let c = this.count;
         while (c--) {
 
             let r = this.radian + Math.PI * 2 / this.count * c;
 
             ctx.beginPath();
-            ctx.arc(Math.cos(r) * this.radius + this.cx, Math.sin(r) * this.radius + this.cy, 20, 0, Math.PI * 2);
+            ctx.arc(
+                Math.cos(r) * (this.radius + this.radiusAjust) + this.cx,
+                Math.sin(r) * (this.radius + this.radiusAjust) + this.cy,
+                this.size,
+                0,
+                Math.PI * 2);
             ctx.closePath();
             ctx.stroke();
+            ctx.fill();
 
         }
 
@@ -52,6 +69,13 @@ ds.setup({
         count : function (e, sys, state) {
 
             sys.count = Math.floor(e.target.value / 100 * 9 + 1);
+
+        },
+
+        // size of circles
+        size : function (e, sys) {
+
+            sys.maxSize = 5 + e.target.value / 100 * 45;
 
         }
 
